24fda3674d57943f16e1ab3775fa48d2cf641aa2
Николай Дымура <kolyav1ampir@gmail.com>
2016-08-16 23:12:02 +0300

    Readme.md и рабочий скрипт на jQuery + форматирование кода

--- js/javascript.clearblock.js
+++
@@ -1,9 +0,0 @@
-/**This code doesn`t work, outside html 
-*This script delete block, in which it is
-
-  * function clear_coment()
-  * */
-('.clear_coment').click(function(e){
-    var index=('.clear_coment').index(this); 
-    (this).parent().remove();
-    }
