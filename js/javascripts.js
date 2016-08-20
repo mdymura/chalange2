8d0db03983afd5ac1a827518b2dcc23e6c792389
Николай Дымура <kolyav1ampir@gmail.com>
2016-08-20 17:20:33 +0300

    беспорядные изменения

--- js/javascripts.js
+++ js/javascripts.js
@@ -1,25 +1,28 @@
 ﻿/**This function show variable Elem(Family,Name,Age,Phone).*/
-        function Complete()
-        {
+       /* function Complete()*/
+       $(document).ready(function () {
+            $('#Complete', '[name=Form1]').click(function(){
             var Elem="Фамилия: " + document.Form1.Family.value +
                 "\nИмя: " + document.Form1.Name.value +
                 "\nВозраст: " + document.Form1.Age.value +
                 "\nТелефон: " + document.Form1.Phone.value;
             alert(Elem);
-        }
+            });
+        
 /**
 This function return age to form.
 @param {int} age - output number
 */        
-        function CheckAge(age)
-        {
+        /*function CheckAge(age)
+        $('.CheckAge').click(function(){
+        
             if(age<18)
                 return "18";
             else
                 return  age;
-        }
         
-   
+        });
+   */})
 
 
 
