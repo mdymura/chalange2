92365c737851f980d2ae056de2aeb9c5fded7b31
Николай Дымура <kolyav1ampir@gmail.com>
2016-08-20 17:32:22 +0300

    работает вроде...

--- js/javascripts.js
+++ js/javascripts.js
@@ -1,19 +1,17 @@
 ﻿/**This function show variable Elem(Family,Name,Age,Phone).*/
-       /* function Complete()*/
-       $(document).ready(function () {
-            $('#Complete', '[name=Form1]').click(function(){
+    $(document).ready(function () {
+        $('#Complete', '[name=Form1]').click(function(){
             var Elem="Фамилия: " + document.Form1.Family.value +
                 "\nИмя: " + document.Form1.Name.value +
                 "\nВозраст: " + document.Form1.Age.value +
                 "\nТелефон: " + document.Form1.Phone.value;
             alert(Elem);
-            });
+        });
         
 /**
 This function return age to form.
 @param {int} age - output number
-*/        
-        /*function CheckAge(age)
+*/          
         $('.CheckAge').click(function(){
         
             if(age<18)
@@ -22,7 +20,7 @@ This function return age to form.
                 return  age;
         
         });
-   */})
+    })
 
 
 
